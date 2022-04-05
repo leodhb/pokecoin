5255c67143af22ca90edecfb687a84b55989ec38
Henrique Barbosa <henrikleod@gmail.com>
2022-04-05 18:46:47 -0300

    Add a devise mock for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        User.where(
+            full_name: "Henrique Barbosa",
+            email: 'henrikleod@gmail.com',
+            password: "singleteste"
+        ).first_or_create
+    end
 end
