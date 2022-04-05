fb0429760e68b1d1fb650741313a8df7bf21177f
Henrique Barbosa <henrikleod@gmail.com>
2022-04-05 00:50:26 -0300

    Pokemon controller and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#index"
+  get '/pokemon', to: 'pokemon#index', as: 'pokemon_index'
+  get '/pokemon/:id', to: 'pokemon#show', as: 'pokemon_show'
+  get '/pokemon/:id/checkout', to: 'pokemon#checkout', as: 'pokemon_checkout'
+  post '/pokemon/:id/buy', to: 'pokemon#buy', as: 'pokemon_buy'
+  post '/pokemon/:id/sell', to: 'pokemon#sell', as: 'pokemon_sell'
 end
