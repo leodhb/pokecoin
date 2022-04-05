4939e591f8a9f076a01cbc9484148c88631a789c
Henrique Barbosa <henrikleod@gmail.com>
2022-04-05 18:34:33 -0300

    Add checkout handlers to controller (we gonna refact it later)

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -8,11 +8,24 @@ class PokemonController < ApplicationController
   end
 
   def checkout
+    @pokemon = Pokemon.find(params[:id])
+    @price_in_usd, @btc_price = btc_to_usd(@pokemon.price)
   end
 
   def buy
   end
   
   def sell
+  end
+
+  def btc_to_usd(pokemon_price)
+    url = "https://api.blockchain.com/v3/exchange/tickers/BTC-USD"
+
+    btc_price = JSON.parse(RestClient.get(url))['price_24h'].to_f
+
+    [
+      btc_price * pokemon_price,
+      btc_price
+    ]
   end
 end
